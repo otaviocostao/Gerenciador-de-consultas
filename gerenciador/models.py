7017e27a99e0028f4945ee107f66737e39eb1a65
Otavio Costa <dev.otaviocostao@gmail.com>
2024-05-30 23:48:04 -0300

    Mais campos na model, filtros da tela de exibição

--- gerenciador/models.py
+++ gerenciador/models.py
@@ -2,11 +2,56 @@ from django.db import models
 
 # Create your models here.
 class Paciente(models.Model):
+
+    ESPECIALIDADE_UM = 'Clinico Geral'
+    ESPECIALIDADE_DOIS = 'Oftalmologista'
+    ESPECIALIDADE_TRES = 'Dermatologista'
+
+    ESPECIALIDADE_CHOICES = [
+        (ESPECIALIDADE_UM, 'Clinico Geral'),
+        (ESPECIALIDADE_DOIS, 'Oftalmologista'),
+        (ESPECIALIDADE_TRES, 'Dermatologista'),
+    ]
+
+    OPTION_SIM = 'Sim'
+    OPTION_NAO = 'Não'
+
+    OPTION_CHOICES = [
+        (OPTION_SIM, 'Sim'),
+        (OPTION_NAO, 'Não'),
+    ]
+
+    PAGAMENTO_UM = 'Dinheiro'
+    PAGAMENTO_DOIS = 'Cartão de credito'
+    PAGAMENTO_TRES = 'Cartão de debito'
+    PAGAMENTO_QUATRO = 'Convênio médico'
+
+    PAGAMENTO_CHOICES = [
+        (PAGAMENTO_UM, 'Dinheiro'),
+        (PAGAMENTO_DOIS, 'Cartão de credito'),
+        (PAGAMENTO_TRES, 'Cartão de debito'),
+        (PAGAMENTO_QUATRO, 'Convênio médico'),
+    ]
+
+    DIA_SEGUNDA = 'Segunda-feira'
+    DIA_TERCA = 'Terça-feira'
+    DIA_QUARTA = 'Quarta-feira'
+    DIA_QUINTA = 'Quinta-feira'
+    DIA_SEXTA = 'Sexta-feira'
+
+    DIAS_CHOICES = [
+        (DIA_SEGUNDA, 'Segunda-feira'),
+        (DIA_TERCA, 'Terça-feira'),
+        (DIA_QUARTA, 'Quarta-feira'),
+        (DIA_QUINTA, 'Quinta-feira'),
+        (DIA_SEXTA, 'Sexta-feira'),
+    ]
+
     nome = models.CharField(max_length=100,null=False, blank=False)
 
     endereco = models.CharField(max_length=100)
 
-    prioridade = models.CharField(max_length=5, null=False, blank=False)
+    prioridade = models.CharField(max_length=5, null=False, blank=False, choices=OPTION_CHOICES, default=OPTION_NAO)
 
     cpf = models.CharField(max_length=15, null=False, blank=False)
 
@@ -17,3 +62,13 @@ class Paciente(models.Model):
     email = models.CharField(max_length=100)
 
     data_nascimento = models.DateField(null=False, blank=False)
+
+    # Dados da consulta:
+
+    especialidade = models.CharField(max_length=50, null=False, blank=False, choices=ESPECIALIDADE_CHOICES, default=ESPECIALIDADE_UM)
+
+    data_consulta = models.CharField(max_length=50, null=False, blank=False, choices=DIAS_CHOICES)
+
+    convenio_medico = models.CharField(max_length=50, null=False, blank=False, choices=OPTION_CHOICES, default=OPTION_NAO)
+
+    forma_pagamento = models.CharField(max_length=50, null=False, blank=False, choices=PAGAMENTO_CHOICES, default=PAGAMENTO_UM)
--- gerenciador/views.py
+++ gerenciador/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from django.views.generic import TemplateView, CreateView, DetailView
 from gerenciador.models import Paciente
 from django.urls import reverse_lazy
 # Create your views here.
@@ -12,15 +12,27 @@ class HomeTemplateView(TemplateView):
 
 class AgendarPaciente(CreateView):
     model=Paciente
-    fields=['nome', 'endereco', 'cpf', 'rg', 'telefone', 'email', 'prioridade', 'data_nascimento']
+    fields=['nome', 'endereco', 'cpf', 'rg', 'telefone', 'email', 'data_nascimento', 'prioridade', 'especialidade', 'data_consulta', 'convenio_medico', 'forma_pagamento']
     template_name='gerenciador/agendar.html'
     def get_success_url(self):
         return reverse_lazy('dados_consulta', kwargs={'pk': self.object.pk})
 
-class ListaPacientes(ListView):
-    model=Paciente
-    template_name='gerenciador/ver_consultas.html'
-    context_object_name = 'pacientes'
+def ver_consultas(request):
+    especialidade = request.GET.get('especialidade')
+    data_consulta = request.GET.get('data_consulta')
+    
+    # Filtragem inicial de todos os pacientes
+    pacientes = Paciente.objects.all()
+    
+    # Filtragem por especialidade, se especificada
+    if especialidade:
+        pacientes = pacientes.filter(especialidade=especialidade)
+    
+    # Filtragem por data de consulta, se especificada
+    if data_consulta:
+        pacientes = pacientes.filter(data_consulta=data_consulta)
+    
+    return render(request, 'gerenciador/ver_consultas.html', {'pacientes': pacientes})
 
 class DadosConsulta(DetailView):
     model= Paciente
