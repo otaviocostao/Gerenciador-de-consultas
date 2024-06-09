882069b58e4dad3d8277e07d00c840b76f1c60ce
Otavio <dev.otaviocostao@gmail.com>
2024-06-09 13:28:14 -0300

    Requisição de autenticação e calendario na tela home

--- gerenciador/views.py
+++ gerenciador/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
 from gerenciador.models import Paciente, PacienteFilaEspera
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
 
 def login(request): # VIEW DA TELA DE LOGIN
     if request.method == "GET": # CASO O LOGIN JA TENHA SIDO FEITO
@@ -26,7 +28,7 @@ class HomeTemplateView(LoginRequiredMixin, TemplateView): # VIEW DA HOMEPAGE
     login_url= 'login' # REQUISIÇÃO DO LOGIN
     redirect_field_name= 'redirect_to'
 
-class AgendarPaciente(CreateView):
+class AgendarPaciente(LoginRequiredMixin, CreateView):
     model = Paciente
     template_name = 'gerenciador/agendar.html'
     fields = [
@@ -34,6 +36,8 @@ class AgendarPaciente(CreateView):
         'data_nascimento', 'prioridade', 'especialidade',
         'data_consulta', 'convenio_medico', 'forma_pagamento'
     ]
+    login_url= 'login' # REQUISIÇÃO DO LOGIN
+    redirect_field_name= 'redirect_to'
 
     def form_valid(self, form):
         especialidade = form.cleaned_data['especialidade']
@@ -65,7 +69,8 @@ class AgendarPaciente(CreateView):
         if self.request.is_ajax():
             return JsonResponse(form.errors, status=400)
         return super().form_invalid(form)
-        
+    
+@login_required(login_url=reverse_lazy('login'))
 def ver_consultas(request): # VIEW PARA VISUALIZAR AS CONSULTAS MARCADAS
     especialidade = request.GET.get('especialidade') # QUERY DA ESPECIALIDADE NA BASE DE DADOS
     data_consulta = request.GET.get('data_consulta') # QUERY DO DIA NA BASE DE DADOS
@@ -82,20 +87,25 @@ def ver_consultas(request): # VIEW PARA VISUALIZAR AS CONSULTAS MARCADAS
     
     return render(request, 'gerenciador/ver_consultas.html', {'pacientes': pacientes}) # RENDER DA VIEW
 
-class DadosConsulta(DetailView): # VIEW DE CONFIRMAÇÃO DA CONSULTA
+class DadosConsulta(LoginRequiredMixin, DetailView): # VIEW DE CONFIRMAÇÃO DA CONSULTA
     model= Paciente # TABELA DA BASE DE DADOS
     template_name= 'gerenciador/dados_consulta.html' # PASSAGEM DA TEMPLATE HTML
     context_object_name= 'paciente' # APELIDO DA TABELA NO TEMPLATE
+    login_url= 'login' # REQUISIÇÃO DO LOGIN
+    redirect_field_name= 'redirect_to'
 
-class EditarFicha(UpdateView): # VIEW PARA EDITAR A FICHA DO PACIENTE A PARTIR DO SEU ID PRIMARY KEY
+class EditarFicha(LoginRequiredMixin, UpdateView): # VIEW PARA EDITAR A FICHA DO PACIENTE A PARTIR DO SEU ID PRIMARY KEY
     model=Paciente  # TABELA DA BASE DE DADOS
     fields=['nome', 'endereco', 'cpf', 'rg', 'telefone', 'email', 'data_nascimento',
             'prioridade', 'especialidade', 'data_consulta', 'convenio_medico', 'forma_pagamento'] # CAMPOS DO FORMULARIO HTML
     template_name='gerenciador/editar.html'# PASSAGEM DA TEMPLATE HTML
-    
+    login_url= 'login' # REQUISIÇÃO DO LOGIN
+    redirect_field_name= 'redirect_to'
+
     def get_success_url(self): # URL USADA PARA REDIRECIONAR APÓS SALVAR A ALTERAÇÃO
         return reverse_lazy('consultas')
     
+@login_required(login_url=reverse_lazy('login'))
 def desmarcar_consulta(request, pk): 
     paciente = get_object_or_404(Paciente, pk=pk) 
     especialidade = paciente.especialidade
@@ -128,7 +138,7 @@ def desmarcar_consulta(request, pk):
 
     return redirect('consultas')
 
-class Agendar_Fila(CreateView):
+class Agendar_Fila(LoginRequiredMixin, CreateView):
     model = PacienteFilaEspera
     template_name = 'gerenciador/agendar_filaespera.html'
     fields = [
@@ -136,11 +146,13 @@ class Agendar_Fila(CreateView):
         'data_nascimento_fila', 'prioridade_fila', 'especialidade_fila',
         'data_consulta_fila', 'convenio_medico_fila', 'forma_pagamento_fila'
     ]
+    login_url= 'login' # REQUISIÇÃO DO LOGIN
+    redirect_field_name= 'redirect_to'
 
     def get_success_url(self):
         return reverse_lazy('fila_espera')
 
-
+@login_required(login_url=reverse_lazy('login'))
 def ListarFiladeEspera(request):
     especialidade_fila = request.GET.get('especialidade_fila') # QUERY DA ESPECIALIDADE NA BASE DE DADOS
     data_consulta_fila = request.GET.get('data_consulta_fila') # QUERY DO DIA NA BASE DE DADOS
@@ -158,11 +170,13 @@ def ListarFiladeEspera(request):
     return render(request, 'gerenciador/ver_fila.html', {'pacientes_fila': pacientes_fila}) # RENDER DA VIEW
 
 
-class EditarFichaFila(UpdateView): # VIEW PARA EDITAR A FICHA DO PACIENTE A PARTIR DO SEU ID PRIMARY KEY
+class EditarFichaFila(LoginRequiredMixin, UpdateView): # VIEW PARA EDITAR A FICHA DO PACIENTE A PARTIR DO SEU ID PRIMARY KEY
     model=PacienteFilaEspera  # TABELA DA BASE DE DADOS
     fields=['nome_fila', 'endereco_fila', 'cpf_fila', 'rg_fila', 'telefone_fila', 'email_fila', 'data_nascimento_fila',
             'prioridade_fila', 'especialidade_fila', 'data_consulta_fila', 'convenio_medico_fila', 'forma_pagamento_fila'] # CAMPOS DO FORMULARIO HTML
     template_name='gerenciador/editar_fila.html'# PASSAGEM DA TEMPLATE HTML
-    
+    login_url= 'login' # REQUISIÇÃO DO LOGIN
+    redirect_field_name= 'redirect_to'
+
     def get_success_url(self): # URL USADA PARA REDIRECIONAR APÓS SALVAR A ALTERAÇÃO
         return reverse_lazy('fila_espera')
